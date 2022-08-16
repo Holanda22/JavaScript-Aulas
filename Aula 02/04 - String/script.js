5f4f5d0ae243ac17f9aeec205761143a141c91c4
Beatriz Holanda <102962811+Holanda22@users.noreply.github.com>
2022-08-16 10:36:26 -0300

    Aula02

---
+++ Aula 02/04 - String/script.js
@@ -0,0 +1,15 @@
+console.log("Isso é uma String!");
+console.log('Isso também é uma String!');
+console.log(`E isso também`); //template literals
+
+//uso de barra (\)
+console.log("Essa é uma String \n de duas linhas");
+
+//Uso de template literals
+console.log(`A soma de 2+2 é ${2+2}`);
+
+//Uso da concatenação
+console.log("Salada" + " de " + "frutas");
+
+
+
